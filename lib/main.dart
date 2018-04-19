7337f2998b88d69f950a6602dcec5f9b63933280
leyan95 <18231926271@163.com>
2018-04-20 06:37:04 +0800

    调成程序结构。

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:helloworld/MyScaffold.dart';
 
 void main() => runApp(new MaterialApp(
       title: 'MyApp',
       home: new MyScaffold(),
     ));
-
-class MyAppBar extends StatelessWidget {
-  MyAppBar({this.title});
-
-  final Widget title;
-
-  @override
-  Widget build(BuildContext context) {
-    return new Container(
-      height: 56.0,
-      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      decoration: new BoxDecoration(color: Colors.blue[500]),
-      child: new Row(
-        children: <Widget>[
-          new IconButton(
-              icon: new Icon(Icons.menu),
-              tooltip: 'Navigation menu',
-              onPressed: null),
-          new Expanded(child: title),
-          new IconButton(
-              icon: new Icon(Icons.search), tooltip: 'search', onPressed: null)
-        ],
-      ),
-    );
-  }
-}
-
-class MyScaffold extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return new Material(
-      child: new Column(
-        children: <Widget>[
-          new MyAppBar(
-              title: new Text(
-            'Example title',
-            style: Theme.of(context).primaryTextTheme.title,
-          )),
-          new Expanded(
-              child: new Center(
-            child: new Text('Hello World.'),
-          )),
-        ],
-      ),
-    );
-  }
-}
