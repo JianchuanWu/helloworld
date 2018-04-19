ccedd7c79ba1fdb0d51e38486aafe8901f449a8e
leyan95 <18231926271@163.com>
2018-04-20 06:46:09 +0800

    使用 Material 组件。

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:helloworld/MyScaffold.dart';
+import 'package:helloworld/TutorialHome.dart';
 
 void main() => runApp(new MaterialApp(
       title: 'MyApp',
-      home: new MyScaffold(),
+      home: new TutorialHome(),
     ));
