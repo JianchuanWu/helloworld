3a303be05d817b644dc77e13b8c873e255c818aa
leyan95 <18231926271@163.com>
2018-04-21 07:51:18 +0800

    实现简单购物车功能。

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:helloworld/MyButton.dart';
+import 'package:helloworld/ShoppingList.dart';
+import 'package:helloworld/ShoppingListItem.dart';
 
 void main() => runApp(new MaterialApp(
       title: 'MyApp',
-      home: new MyButton(),
+      home: new ShoppingList(
+        products: <Product>[
+          new Product(name: 'Eggs'),
+          new Product(name: 'Flour'),
+          new Product(name: 'Chocolate chips'),
+        ],
+      ),
     ));
