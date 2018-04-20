b9862be72a0bfc2e4f6330bd41017d3b00d222b3
leyan95 <18231926271@163.com>
2018-04-21 07:05:43 +0800

    分离显示计数器和更改计数器的逻辑。

--- lib/Counter.dart
+++ lib/Counter.dart
@@ -9,19 +9,43 @@ class _CounterState extends State<Counter> {
   int _counter = 0;
 
   void _increment() {
-    setState(() => _counter++);
+    setState(() {
+      ++_counter;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return new Row(
       children: <Widget>[
-        new RaisedButton(
-          onPressed: _increment,
-          child: new Text('Increment'),
-        ),
-        new Text('_Count:$_counter')
+        new CounterIncrementor(onPressed: _increment),
+        new CounterDisplay(count: _counter)
       ],
+    );
+  }
+}
+
+class CounterDisplay extends StatelessWidget {
+  CounterDisplay({this.count});
+
+  final int count;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Text('count:$count');
+  }
+}
+
+class CounterIncrementor extends StatelessWidget {
+  CounterIncrementor({this.onPressed});
+
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return new RaisedButton(
+      onPressed: onPressed,
+      child: new Text('Increment'),
     );
   }
 }
