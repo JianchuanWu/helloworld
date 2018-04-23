f69f5dadbed1587e085224a777a5c84c000087e0
leyan95 <18231926271@163.com>
2018-04-23 21:37:01 +0800

    使用不同类型的子项创建列表。

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:helloworld/MyLongListApp.dart';
+import 'package:helloworld/MixedList.dart';
 
-void main() => runApp(new MyLongListApp(
-      items: new List<String>.generate(10000, (i) => "Item $i"),
+void main() => runApp(new MixedList(
+      items: new List<ListItem>.generate(
+          1000,
+          (i) => i % 6 == 0
+              ? new HeadingItem("Heading $i")
+              : new MessageItem('Sender $i,', 'Message body $i')),
     ));
