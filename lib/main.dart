5b0a5f75368f1d8c14cefc67f04a372a17fbe86b
leyan95 <18231926271@163.com>
2018-04-23 21:46:24 +0800

    使用GridView.count构造函数创建一个 Grid List。

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:helloworld/MixedList.dart';
+import 'package:helloworld/GridList.dart';
 
-void main() => runApp(new MixedList(
-      items: new List<ListItem>.generate(
-          1000,
-          (i) => i % 6 == 0
-              ? new HeadingItem("Heading $i")
-              : new MessageItem('Sender $i,', 'Message body $i')),
-    ));
+void main() => runApp(new GridList());
