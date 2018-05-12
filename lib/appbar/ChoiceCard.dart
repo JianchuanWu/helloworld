a4c5c8da912ac640b299783301214065ccfe8a39
leyan95 <18231926271@163.com>
2018-05-12 22:22:25 +0800

    一个典型的AppBar，带有标题、操作和溢出的下拉菜单。

---
+++ lib/appbar/ChoiceCard.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:helloworld/appbar/Choice.dart';
+
+class ChoiceCard extends StatelessWidget {
+  const ChoiceCard({ Key key, this.choice }) : super(key: key);
+
+  final Choice choice;
+
+  @override
+  Widget build(BuildContext context) {
+    final TextStyle textStyle = Theme.of(context).textTheme.display1;
+    return new Card(
+      color: Colors.white,
+      child: new Center(
+        child: new Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            new Icon(choice.icon, size: 128.0, color: textStyle.color),
+            new Text(choice.title, style: textStyle),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/appbar/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:helloworld/appbar/BasicAppBarSample.dart';
+
+void main() {
+  runApp(new BasicAppBarSample());
+}
